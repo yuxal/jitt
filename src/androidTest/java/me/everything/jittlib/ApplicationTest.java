d191ab0a06ac14dee5a95c3105f4fbc29eabb396
Adam Kariv <adam@everything.me>
2014-12-09 18:05:59 +0200

    Adding translation API

--- src/androidTest/java/me/everything/jittlib/ApplicationTest.java
+++ src/androidTest/java/me/everything/jittlib/ApplicationTest.java
@@ -2,14 +2,60 @@ package me.everything.jittlib;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+    private ServerAPI mServerAPI;
+
     public ApplicationTest() {
         super(Application.class);
     }
 
-    void test
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mServerAPI = new ServerAPI();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        mServerAPI = null;
+    }
+
+    public void testServerAPI() {
+        List<String> keys = new ArrayList<String>();
+        keys.add("s_aaa");
+        keys.add("s_bbb");
+        keys.add("hello_world");
+        List<String> locales = new ArrayList<String>();
+        locales.add("kl_KL");
+        locales.add("en_US");
+        locales.add("es_CL");
+        locales.add("he_IL");
+        final CountDownLatch latch = new CountDownLatch(1);
+        mServerAPI.getTranslations("12345", keys, locales, new ServerAPI.ITranslationsReceiver() {
+            @Override
+            public void onGotTranslations(ServerAPI.TranslationResult suggestions) {
+                Log.e("TTT", "GOT "+suggestions);
+                latch.countDown();
+            }
+        });
+        Log.e("TTT", "WAITING");
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Log.e("TTT", "DONE");
+    }
+
 }
--- src/main/java/me/everything/jittlib/ServerAPI.java
+++ src/main/java/me/everything/jittlib/ServerAPI.java
@@ -1,8 +1,16 @@
 package me.everything.jittlib;
 
 import android.net.Uri;
+import android.util.Log;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -11,24 +19,60 @@ import java.util.Map;
  */
 public class ServerAPI {
 
+    private static final String TAG ="JITT.serverapi";
+    private Gson mGson;
+
     public static class Suggestion {
-        String suggestion;
+        String suggested;
         String user_selected;
         Integer votes;
+
+        public String toString() {
+            return suggested+" ("+votes+" votes, "+user_selected+")";
+        }
     }
 
+    public static class TranslationResult extends HashMap<String,HashMap<String,ArrayList<Suggestion>>> {
+    }
 
     public interface ITranslationsReceiver {
-        public void onGotTranslations( Map<String,Map<String,Suggestion>> suggestions );
+        public void onGotTranslations( TranslationResult suggestions );
     }
 
     public interface IActionDoneReceiver {
         public void onDone( boolean success );
     }
 
+    public ServerAPI() {
+        mGson = new Gson();
+    }
+
+    private String readFromURL(String _url) {
+        InputStreamReader inStreamReader = null;
+        BufferedReader bufferedReader;
+        try {
+            URL url = new URL(_url);
+            inStreamReader = new InputStreamReader(url.openStream());
+            bufferedReader = new BufferedReader(inStreamReader);
+            String data = "";
+            while ( true ) {
+                String line = bufferedReader.readLine();
+                if ( line == null ) {
+                    break;
+                }
+                data += line+"\n";
+            }
+            return data.trim();
+
+        } catch (IOException e) {
+            Log.d(TAG, "Error: " + e);
+        }
+        return null;
+    }
+
     void getTranslations( String deviceId, List<String> keys, List<String> translationLangs,
                           ITranslationsReceiver receiver ) {
-        Uri.Builder builder = Uri.parse("http://jitt-server.appspot.com").buildUpon();
+        Uri.Builder builder = Uri.parse("http://jitt-server.appspot.com/api/translations").buildUpon();
         builder.appendQueryParameter("device_id",deviceId);
         for ( String key : keys ) {
             builder.appendQueryParameter("key", key);
@@ -36,6 +80,11 @@ public class ServerAPI {
         for ( String lang : translationLangs ) {
             builder.appendQueryParameter("locale", lang);
         }
+        Log.d(TAG, "URL=" + builder.toString());
+        String data = readFromURL(builder.toString());
+        Log.v(TAG, "DATA=" + data);
+        TranslationResult parsed = mGson.fromJson(data, TranslationResult.class);
+        receiver.onGotTranslations(parsed);
     }
 
     public void doAction( String deviceId, String action, String key, String lang, String suggestion,
